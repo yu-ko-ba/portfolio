971f2adf6f65cc68d0cad08be08ac8534c930cfc
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2025-02-24 16:19:11 +0900

    feat(ui): implement BottomNavigationBar

---
+++ composeApp/src/wasmJsMain/kotlin/dev/yuyuyuyuyu/portfolio/ui/components/types/Destination.kt
@@ -0,0 +1,9 @@
+package dev.yuyuyuyuyu.portfolio.ui.components.types
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class Destination(
+    val label: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+)
--- composeApp/src/wasmJsMain/kotlin/dev/yuyuyuyuyu/portfolio/ui/portfolio/screens/PortfolioScreen.kt
+++ composeApp/src/wasmJsMain/kotlin/dev/yuyuyuyuyu/portfolio/ui/portfolio/screens/PortfolioScreen.kt
@@ -1,12 +1,41 @@
 package dev.yuyuyuyuyu.portfolio.ui.portfolio.screens
 
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Apps
+import androidx.compose.material.icons.filled.Book
+import androidx.compose.material.icons.outlined.Apps
+import androidx.compose.material.icons.outlined.Book
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import dev.yuyuyuyuyu.portfolio.ui.components.BottomNavigationBar
+import dev.yuyuyuyuyu.portfolio.ui.components.types.Destination
 
 @Composable
 fun PortfolioScreen(
     modifier: Modifier = Modifier,
 ) {
-    Text("PortfolioScreen")
+    val destinations = listOf(
+        Destination(
+            label = "アプリ",
+            selectedIcon = Icons.Filled.Apps,
+            unselectedIcon = Icons.Outlined.Apps,
+        ),
+        Destination(
+            label = "ライブラリ",
+            selectedIcon = Icons.Filled.Book,
+            unselectedIcon = Icons.Outlined.Book,
+        ),
+    )
+
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar(destinations = destinations)
+        }
+    ) { innerPadding ->
+        TODO()
+    }
 }
