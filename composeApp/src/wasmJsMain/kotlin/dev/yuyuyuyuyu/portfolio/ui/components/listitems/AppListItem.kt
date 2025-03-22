ad8f42a6040629282a85fa80b23151e41d1c78b5
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2025-03-22 11:58:59 +0900

    feat(ui): add icons to libraries, plugins and CLI tools

--- composeApp/src/wasmJsMain/kotlin/dev/yuyuyuyuyu/portfolio/ui/components/listitems/AppListItem.kt
+++ composeApp/src/wasmJsMain/kotlin/dev/yuyuyuyuyu/portfolio/ui/components/listitems/AppListItem.kt
@@ -1,108 +1,76 @@
 package dev.yuyuyuyuyu.portfolio.ui.components.listitems
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.OpenInNew
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
 import dev.yuyuyuyuyu.portfolio.data.types.App
-import dev.yuyuyuyuyu.portfolio.ui.components.ExpandMoreIcon
+import dev.yuyuyuyuyu.portfolio.ui.components.listitems.share.BaseListItem
 import dev.yuyuyuyuyu.portfolio.ui.components.listitems.share.ItemBody
-import dev.yuyuyuyuyu.portfolio.ui.components.listitems.share.SupportingContent
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun AppListItem(
     app: App,
     modifier: Modifier = Modifier,
-) {
-    val uriHandler = LocalUriHandler.current
-
-    var expanded by rememberSaveable { mutableStateOf(false) }
-
-    Column(
-        modifier = modifier,
-    ) {
-        ListItem(
-            leadingContent = {
-                Image(
-                    painter = painterResource(app.icon),
-                    contentDescription = "app icon",
-                    modifier = Modifier
-                        .size(40.dp)
-                        .clip(CircleShape)
-                        .background(Color.White),
-                    contentScale = ContentScale.Crop,
-                )
-            },
-            headlineContent = { Text(app.name) },
-            supportingContent = {
-                SupportingContent(
-                    description = app.description,
-                    techStack = app.techStack,
-                )
-            },
-            trailingContent = { ExpandMoreIcon(expanded) },
-            modifier = Modifier.clickable { expanded = !expanded },
+) = BaseListItem(
+    name = app.name,
+    description = app.description,
+    techStackSet = app.techStack,
+    icon = {
+        Image(
+            painter = painterResource(app.icon),
+            contentDescription = "app icon",
+            modifier = it,
+            contentScale = ContentScale.Crop,
         )
-
-        if (expanded) {
-            ItemBody(
-                repositoryUrl = app.repositoryUrl,
-                onSourceCodeLinkClick = { uriHandler.openUri(app.repositoryUrl) },
-            ) {
-                Column(
-                    modifier = Modifier.padding(horizontal = 10.dp),
-                    verticalArrangement = Arrangement.spacedBy(20.dp),
+    },
+    modifier = modifier,
+) { uriHandler ->
+    ItemBody(
+        repositoryUrl = app.repositoryUrl,
+        onSourceCodeLinkClick = { uriHandler.openUri(app.repositoryUrl) },
+    ) {
+        Column(
+            modifier = Modifier.padding(horizontal = 10.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+        ) {
+            if (app.url != null) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
                 ) {
-                    if (app.url != null) {
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.Center,
-                        ) {
-                            Button(
-                                content = {
-                                    Row {
-                                        Icon(Icons.AutoMirrored.Default.OpenInNew, "open in new tab")
-                                        Spacer(Modifier.width(1.dp))
-                                        Text("アプリページを開く")
-                                    }
-                                },
-                                onClick = { uriHandler.openUri(app.url) },
-                            )
-                        }
-                    }
-
-                    if (app.screenshots.isNotEmpty()) {
-                        Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
-                            Text("スクリーンショット")
-
-                            LazyRow(
-                                horizontalArrangement = Arrangement.spacedBy(20.dp),
-                            ) {
-                                items(app.screenshots) { screenshot ->
-                                    Image(painterResource(screenshot), "${app.name}'s screenshot")
-                                }
+                    Button(
+                        content = {
+                            Row {
+                                Icon(Icons.AutoMirrored.Default.OpenInNew, "open in new tab")
+                                Spacer(Modifier.width(1.dp))
+                                Text("アプリページを開く")
                             }
+                        },
+                        onClick = { uriHandler.openUri(app.url) },
+                    )
+                }
+            }
+
+            if (app.screenshots.isNotEmpty()) {
+                Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+                    Text("スクリーンショット")
+
+                    LazyRow(
+                        horizontalArrangement = Arrangement.spacedBy(20.dp),
+                    ) {
+                        items(app.screenshots) { screenshot ->
+                            Image(painterResource(screenshot), "${app.name}'s screenshot")
                         }
                     }
                 }
--- composeApp/src/wasmJsMain/kotlin/dev/yuyuyuyuyu/portfolio/ui/portfolio/screens/CliToolsScreen.kt
+++ composeApp/src/wasmJsMain/kotlin/dev/yuyuyuyuyu/portfolio/ui/portfolio/screens/CliToolsScreen.kt
@@ -6,21 +6,29 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import dev.yuyuyuyuyu.portfolio.data.repositories.CliToolsRepository
 import dev.yuyuyuyuyu.portfolio.ui.components.listitems.ProductListItem
 
 @Composable
 fun CliToolsScreen(
-    cliToolsRepository: CliToolsRepository = CliToolsRepository(),
+    icon: ImageVector,
+    iconDescription: String,
     modifier: Modifier = Modifier,
+    cliToolsRepository: CliToolsRepository = CliToolsRepository(),
 ) = LazyColumn(
     modifier = modifier,
     verticalArrangement = Arrangement.spacedBy(40.dp),
 ) {
     items(cliToolsRepository.getCliTools().sortedBy { it.repositoryUrl }) { product ->
-        ProductListItem(product)
+        ProductListItem(
+            product = product,
+            icon = icon,
+            iconDescription = iconDescription,
+        )
     }
+   
     item {
         Spacer(Modifier)
     }
