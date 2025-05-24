a2f5fe9e3c4528dbcae97cf58cc88b769468b4e1
yu <59269129+yu-ko-ba@users.noreply.github.com>
2025-05-24 12:52:38 +0900

    feat(ui.opensourcelicenselist): eliminate library duplication

--- composeApp/src/commonMain/kotlin/dev/yuyuyuyuyu/portfolio/ui/opensourcelicenselist/OpenSourceLicenseList.kt
+++ composeApp/src/commonMain/kotlin/dev/yuyuyuyuyu/portfolio/ui/opensourcelicenselist/OpenSourceLicenseList.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
 import com.mikepenz.aboutlibraries.ui.compose.rememberLibraries
+import kotlinx.collections.immutable.toImmutableList
 import portfolio.composeapp.generated.resources.Res
 
 @Composable
@@ -14,7 +15,9 @@ fun OpenSourceLicenseList(modifier: Modifier = Modifier) {
     }
 
     LibrariesContainer(
-        libraries = libraries,
+        libraries = libraries?.libraries?.distinctBy { it.name }?.let {
+            libraries?.copy(libraries = it.toImmutableList())
+        },
         modifier = modifier,
         showDescription = true,
     )
